62eda0f7a11da2fcfb7f5936f262d25e4b328fab
MarcosToledo <marcos.toledo@live.com>
2015-08-20 13:24:06 -0300

    classe de colecoes

---
+++ JavaCollections/src/teste/Collections.java
@@ -0,0 +1,14 @@
+package teste;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+ 
+
+public class Collections {
+
+	public static void main(String[] args) {
+		List carros;
+
+	}
+
+}
